b70fa1bca5185267498abb68a1953aa048d795bb
Mumtozbekov <mumtozbekov@gmail.com>
2022-10-19 17:05:12 +0500

    Prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,11 @@
+﻿using Prototype;
+
+var original = new Sheep("Jolly", "Mountain Sheep");
+Console.WriteLine(original.Name); // Jolly
+Console.WriteLine(original.Category); // Mountain Sheep
+
+var cloned = original.Clone();
+cloned.Name = "Dolly";
+Console.WriteLine(cloned.Name); // Dolly
+Console.WriteLine(cloned.Category); // Mountain Sheep
+Console.WriteLine(original.Name); // Jolly
---
+++ Prototype/Sheep.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Sheep
+    {
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public Sheep(string name, string category)
+        {
+            Name = name;
+            Category = category;
+        }
+
+        public Sheep Clone()
+        {
+            return MemberwiseClone() as Sheep;
+        }
+    }
+}
